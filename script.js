0042ea7245ff0143671604f8bc2e4edd863a961d
abhinavcreates <abhinavsinghmikey@gmail.com>
2025-09-05 16:41:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Example: Scroll to top button
+const scrollBtn = document.getElementById("scrollTop");
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+var typed = new Typed(".typed-text", {
+  strings: ["Creative Coder", "Web Developer", "Designer"],
+  typeSpeed: 50,
+  backSpeed: 25,
+  loop: true,
+  showCursor: false
+});
+
+
+
+
+
